118c79dbb22882ae1e051c85042d257d950edd3f
Brandon Kong <bkong28@gmail.com>
2014-05-12 22:09:44 +1000

    Generate Eva Kong app and testingenvironment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Evakong::Application.config.secret_key_base = '37926f4442877225a6848081d9e7d3eda433ba9f078bff108dc72a31c81b233e25ba32bb00f34efce182e425baec0bbd18df38ebe42882b96d4eb51e5ef627d7'
